04de5504e80582d87b3748cc13db77b8a88d8181
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-30 21:03:06 -0300

    Aula 05(06) completada

--- proyecto-bitebank/src/Cuenta.java
+++ proyecto-bitebank/src/Cuenta.java
@@ -1,9 +1,9 @@
 // entidad
 class Cuenta {
 
-	double saldo;
-	int agencia;
-	int numero;
+	private double saldo;
+	private int agencia;
+	private int numero;
 	Cliente titular = new Cliente();
 	
 	// No retorna valor
@@ -29,5 +29,29 @@ class Cuenta {
 		}
 		return false;
 		
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public void setAgencia(int agencia){
+		if(agencia > 0) {
+			this.agencia = agencia;
+		}else {
+			System.out.println("No se permiten valores negativos");
+		}
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
 	}
 }
